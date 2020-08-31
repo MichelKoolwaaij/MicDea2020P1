1f9a4e9ffb85ec20a36e0fbc1d87c30a3027d622
MichelKoolwaaij <michel.koolwaaij@han.nl>
2020-08-31 16:00:59 +0200

    committed initial version

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Michel Koolwaaij on 31-08-20.
+ */
+public class App {
+    public static void main (String[] args){
+        App app = new App();
+        app.start();
+    }
+
+    private void start() {
+        System.out.println("Welkom in de wereld van DEA");
+    }
+}
